25abae13ea4c888839452d1f16f0e2a186855bc8
LoisChen68 <a8283626@gmail.com>
2022-09-03 07:01:19 +0800

    重構種子資料: Record & User seed

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,18 +1,37 @@
-
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Record = require('../record')
+const User = require('../user')
 const SEED_RECORD = require('./record.json')
 
 const db = require('../../config/mongoose')
 
+const SEED_USER = require('./user.json')
 
-db.once('open', () => {
-  Promise.all(Array.from(SEED_RECORD, record => {
-    const { name, date, amount } = record
-    Record.create({
-      name: record.name,
-      date: record.date,
-      amount: record.amount
+
+db.once('open', async () => {
+  await Promise.all(SEED_USER.map(async user => {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(user.password, salt)
+    const userData = await User.create({
+      name: user.name,
+      email: user.email,
+      password: hash
     })
+    await Promise.all(
+      SEED_RECORD.map(async record => {
+        await Record.create({
+          name: record.name,
+          date: record.date,
+          amount: record.amount,
+          user_id: userData._id
+        })
+      }))
+      .then(() => {
+        console.log('Record & User seed created.')
+        process.exit()
+      })
   }))
-  console.log('Record seed created.')
 })
