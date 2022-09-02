4d87f3b9842709677ce865e2af6c06fb50384a2d
LoisChen68 <a8283626@gmail.com>
2022-09-03 01:16:33 +0800

    新增刪除支出功能

--- app.js
+++ app.js
@@ -65,6 +65,13 @@ app.post('/records/:id/edit', (req, res) => {
 })
 
 
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
   console.log(`Express is running on the http://localhost:${port}`)
