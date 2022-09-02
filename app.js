5efabcfcac344c99429f54ff0edf9653d7a00717
LoisChen68 <a8283626@gmail.com>
2022-09-03 01:12:50 +0800

    新增修改支出功能

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Record = require('./models/record')
 const express = require('express')
 const mongoose = require('mongoose')
 const hbs = require('express-handlebars')
+const record = require('./models/record')
 
 const app = express()
 mongoose.connect(process.env.MONGODB_URI)
@@ -19,6 +20,9 @@ db.once('open', () => {
 const port = 3000
 
 app.use(express.urlencoded({ extended: true }))
+
+app.engine('hbs', hbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   Record.find()
@@ -38,8 +42,28 @@ app.post('/records', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.engine('hbs', hbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch(error => console.log(error))
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, date, amount } = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 
 app.listen(port, () => {
