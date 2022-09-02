fdd79781cc68cddc647e57faea6dbfd3f9565d3c
LoisChen68 <a8283626@gmail.com>
2022-09-03 01:59:50 +0800

    引入css設定

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const express = require('express')
 const methodOverride = require('method-override')
 
 const hbs = require('express-handlebars')
-const routes = require('./routes')
 
 const app = express()
+const port = 3000
 
+const routes = require('./routes')
 require('./config/mongoose')
 
-
-
-const port = 3000
+app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }))
 
